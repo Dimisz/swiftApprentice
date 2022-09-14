88e81425158dd1e5ac82ef5de9d4ce713d5d3e67
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-14 10:43:28 +0300

    initial commit

---
+++ FirstSteps/hiThere.playground/Contents.swift
@@ -0,0 +1,19 @@
+// Swift Apprentice Chapter 1 Challenges
+// challenge 1
+/*
+let myAge: Int = 35
+var myDogs: Int = 0
+myDogs += 1
+print(myDogs)
+ */
+
+// challenge 2
+/*
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+ */
+
+// challenge 3
+
