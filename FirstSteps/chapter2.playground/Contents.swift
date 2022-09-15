7cde1a76114af6eaa19fd992e0aa0bd72262a9d0
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-15 22:44:35 +0300

    Data Types practice

---
+++ FirstSteps/chapter2.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+// mini exercises 1
+let age1: Int = 42
+let age2: Int = 21
+let avg1 = Double(age1 + age2) / 2
+print(avg1)
+
+// mini exercises 2
+let firstName: String = "Vladimir"
+let lastName: String = "Solovyov"
+let fullName: String = firstName + " " + lastName
+let myDetails: String = "Hello, my name is \(fullName)"
+print(myDetails)
