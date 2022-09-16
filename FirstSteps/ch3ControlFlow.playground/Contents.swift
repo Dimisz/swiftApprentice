2b9c66e14acee7108b4490eecce480ce1a2e615f
Vladimir Solovyov <leilyngale@icloud.com>
2022-09-16 16:18:04 +0300

    Chapter 3: Loops

--- FirstSteps/ch3ControlFlow.playground/Contents.swift
+++ FirstSteps/ch3ControlFlow.playground/Contents.swift
@@ -21,6 +21,7 @@ print("Does author come before reader: \(authorBeforeReader)")
 */
 
 // if-else
+/*
 let myAge: Int = 35
 if (myAge >= 13) && (myAge <= 19){
     print("Teenager")
@@ -31,3 +32,23 @@ else {
 
 let myAnswer: String = (myAge >= 13) && (myAge <= 19) ? "Teenager" : "Not a teenager"
 print(myAnswer)
+*/
+
+// loops
+/*
+var counter: Int = 0
+while counter < 10 {
+    print("Counter is \(counter)")
+    counter += 1
+}
+
+var count: Int = 0
+var roll: Int = 0
+repeat {
+    roll = Int.random(in: 0...5)
+    count += 1
+    print("After \(count) rolls the roll is \(roll)")
+} while roll > 0
+*/
+
+
